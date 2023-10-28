8bef12ab1b6258ef6ca7fa2f9639e3d3a48c69d7
xennia87 <62959606+xennia87@users.noreply.github.com>
2023-10-28 14:26:49 +0200

    "HTML Carrito finalizado"

---
+++ main.js
@@ -0,0 +1,38 @@
+//// Cosas que hay que hacer
+
+//// ACTIVIDAD
+
+// - Captura de eventos
+// - Salida en el HTML
+// - Almacenar datos en el storage
+// - Recuperar datos del storage
+// - No se puede usar alert() para salida
+// - No se puede usar prompt() para entrada
+// - Capturamos eventos usuario inputs sobre botones
+
+//// COSAS EN LA PAGINA
+
+
+// LOGIN
+
+// 1. Hacer que cuando el usuario se loguea con user y password "admin"
+// la tab "Crear artículo" aparezca
+
+// CREAR ARTÍCULO
+
+// Crear un form flotante para crear un artículo con las opciones:
+// - Nombre, - Descripción, - Precio e -Imagen
+
+// Cuando el admin cree un articulo, este debe aparecer como una card
+// en la página.
+// Debe añadirse al archivo JSON?
+
+// CARRITO
+
+// Cuando el usuario pulse en el botón añadir al carrito, el item
+// debe aparecer ahí.
+
+// Cuando el usuario pulse en el botón de "quitar" el item debe eliminarse
+
+// El total del carrito se debe calcular según el precio de los elementos que
+// haya en él
