9433c15973dd0ef09e3746193f03c015b1391bae
xennia87 <62959606+xennia87@users.noreply.github.com>
2023-10-29 18:21:09 +0100

    "Arreglados elementos de CSS"

--- main.js
+++ main.js
@@ -42,74 +42,73 @@
 /// MENSAJE BIENVENIDA
 
 // Función para mostrar el mensaje de bienvenida
-function mostrarMensajeBienvenida(username) {
-    const headerMessageContainer = document.getElementById('headerMessageContainer');
-    const headerMessage = document.getElementById('headerMessage');
-    const closeMessage = document.getElementById('closeMessage');
-    headerMessageContainer.style.display = 'flex';
+function showWelcomeMessage(username) {
+    const headerMessageContainer = document.getElementById('headerMessageContainer')
+    const headerMessage = document.getElementById('headerMessage')
+    const closeMessage = document.getElementById('closeMessage')
+    headerMessageContainer.style.display = 'flex'
     headerMessage.innerText = (username == 'admin') ? `Bienvenido ${username}.
-    Puedes crear artículos desde el enlace superior` : `Bienvenido ${username}`;
+    Puedes crear artículos desde el enlace superior` : `Bienvenido ${username}`
     
     // Cierra el mensaje de bienvenida cuando se hace clic en el botón de cierre
     closeMessage.addEventListener('click', () => {
-        headerMessageContainer.style.display = 'none';
-    });
+        headerMessageContainer.style.display = 'none'
+    })
 }
 /// SISTEMA LOGIN
 
 // Cogemos el json de usuarios, lo pasamos a un array
 // y lo convertimos en objeto
 
-const usuarios = '[ { "username": "admin", "password": "admin" }, { "username": "enduser", "password": "enduser" } ]'
+const users = '[ { "username": "admin", "password": "admin" }, { "username": "enduser", "password": "enduser" } ]'
 
-const usuariosObj = JSON.parse(usuarios);
+const usersObj = JSON.parse(users)
 
 // Obtenemos los elementos del DOM
-const loginButton = document.getElementById("loginButton");
-const modal = document.getElementById("modalweb");
-const close = document.getElementById("close");
+const loginButton = document.getElementById("loginButton")
+const modal = document.getElementById("modalweb")
+const close = document.getElementById("close")
 
 // Mostramos el modal cuando se hace clic en el botón "Login"
 loginButton.addEventListener("click", () => {
-  modal.style.display = "block";
-});
+  modal.style.display = "block"
+})
 
 // Ocultamos el modal cuando se hace clic en la "X" de cerrar
 close.addEventListener("click", () => {
-  modal.style.display = "none";
-});
+  modal.style.display = "none"
+})
 
 // Ocultamos el modal cuando se hace clic fuera de él
 window.addEventListener("click", (event) => {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.display = "none"
   }
-});
-
+})
 
 // Función para verificar las credenciales
-function verificarCredenciales(username, password) {
-    const usuarioEncontrado = usuariosObj.find(user => user.username === username && user.password === password);
-    const tabCrearArticulo = document.getElementById('crear_articulo')
+function verifyCredentials(username, password) {
+    const userFound = usersObj.find(user => user.username === username && user.password === password)
+    const createArticleTab = document.getElementById('crear_articulo')
 
-    if (usuarioEncontrado) {
-        mostrarMensajeBienvenida(usuarioEncontrado.username); // Muestra el mensaje de bienvenida
-        tabCrearArticulo.style.display = 'block';
-        modal.style.display = "none"; // Cierra el modal
+    if (userFound) {
+        showWelcomeMessage(userFound.username) // Muestra el mensaje de bienvenida
+        createArticleTab.style.display = 'block'
+        modal.style.display = "none" // Cierra el modal
     } else {
         // Usuario no encontrado, muestra un mensaje de error en el modal
-        document.getElementById('errorMessage').textContent = 'Usuario no encontrado';
-        document.getElementById('errorMessage').style.color = 'red';
+        document.getElementById('errorMessage').textContent = 'Usuario no encontrado'
+        document.getElementById('errorMessage').style.color = 'red'
     }
 }
 
 // Manejar el evento de inicio de sesión
 document.getElementById('login').addEventListener("click", () => {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
 
-    verificarCredenciales(username, password);
-});
+    verifyCredentials(username, password)
+})
 
 /// CONTENEDOR ARTICULOS
 
@@ -117,198 +116,172 @@ document.getElementById('login').addEventListener("click", () => {
 
 const cardsContainer = document.getElementById('cards_container')
 
-class nuevoItem {
-    constructor(nombre, descripcion, precio, imagen) {
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.imagen = imagen;
+class cardItem {
+    constructor(name, description, price, image) {
+        this.name = name
+        this.description = description
+        this.price = price
+        this.image = image
         // Esta función agrega el item al DOM
-        this.agregarItem = function() {
+        this.addItem = function() {
             // Creamos todos los elementos y le damos el contenido
-            const nuevoItem = document.createElement('div')
-            nuevoItem.classList.add('card')
+            const newItem = document.createElement('div')
+            newItem.classList.add('card')
 
-            const nuevoItemImagen = document.createElement('div')
-            nuevoItemImagen.classList.add('card_image')
+            const newItemImagen = document.createElement('div')
+            newItemImagen.classList.add('card_image')
             const imagenDentroDiv = document.createElement('img')
-            imagenDentroDiv.src = this.imagen
+            imagenDentroDiv.src = this.image
 
-            const nuevoItemText = document.createElement('div')
-            nuevoItemText.classList.add('card_text')
-            const nuevoItemTitle = document.createElement('div')
-            nuevoItemTitle.classList.add('card_title')
-            nuevoItemTitle.textContent = this.nombre
+            const newItemText = document.createElement('div')
+            newItemText.classList.add('card_text')
+            const newItemTitle = document.createElement('div')
+            newItemTitle.classList.add('card_title')
+            newItemTitle.textContent = this.name
 
-            const nuevoItemPrice = document.createElement('div')
-            nuevoItemPrice.classList.add('card_price')
-            nuevoItemPrice.textContent = this.precio
+            const newItemPrice = document.createElement('div')
+            newItemPrice.classList.add('card_price')
+            newItemPrice.textContent = this.price
 
-            const nuevoItemDescription = document.createElement('div')
-            nuevoItemDescription.classList.add('card_description')
-            nuevoItemDescription.textContent = this.descripcion
+            const newItemDescription = document.createElement('div')
+            newItemDescription.classList.add('card_description')
+            newItemDescription.textContent = this.description
 
-            const nuevoItemButton = document.createElement('button')
-            nuevoItemButton.classList.add('card_submit')
-            nuevoItemButton.id = 'boton_agregar'
-            nuevoItemButton.textContent = 'Añadir al carrito'
+            const newItemButton = document.createElement('button')
+            newItemButton.classList.add('card_submit')
+            newItemButton.id = 'boton_agregar'
+            newItemButton.textContent = 'Añadir al carrito'
 
             //Añadimos los elementos creados
-            nuevoItemImagen.appendChild(imagenDentroDiv)
-            nuevoItemText.appendChild(nuevoItemTitle)
-            nuevoItemText.appendChild(nuevoItemPrice)
-            nuevoItemText.appendChild(nuevoItemDescription)
-            nuevoItemText.appendChild(nuevoItemButton)
+            newItemImagen.appendChild(imagenDentroDiv)
+            newItemText.appendChild(newItemTitle)
+            newItemText.appendChild(newItemPrice)
+            newItemText.appendChild(newItemDescription)
+            newItemText.appendChild(newItemButton)
             
-            nuevoItem.appendChild(nuevoItemImagen)
-            nuevoItem.appendChild(nuevoItemText)
+            newItem.appendChild(newItemImagen)
+            newItem.appendChild(newItemText)
 
-            cardsContainer.appendChild(nuevoItem)
+            cardsContainer.appendChild(newItem)
         }
     }
 }
 
 // Agregamos nuevos items
 
-let pastel = new nuevoItem("Pastel Rico", "El pastel más delicioso", 7.50, "images/cake.jpeg")
-pastel.agregarItem()
+let pastel = new cardItem("Pastel Rico", "El pastel más delicioso", 7.50, "images/cake.jpeg")
+pastel.addItem()
 
-let sandwich = new nuevoItem("Neko sandwich", "Un sandwich de atun y sardinas", 8, "images/sandwich.jpeg")
-sandwich.agregarItem()
+let sandwich = new cardItem("Neko sandwich", "Sandwich de atun y sardinas", 8, "images/sandwich.jpeg")
+sandwich.addItem()
 
 const sandwichString = JSON.stringify(sandwich)
 
 /// CARRITO
-
-function obtenerCarrito() {
-    const carritoJSON = localStorage.getItem('carrito')
-    return JSON.parse(carritoJSON) || []
-}
-
-function guardarCarrito(carrito) {
-    const carritoJSON = JSON.stringify(carrito)
-    localStorage.setItem('carrito', carritoJSON)
-    console.log('Guardar carrito' + carritoJSON)
-}
-
-let carrito = obtenerCarrito()
-
 // Agregar al carrito
 
 const cartContainer = document.getElementById('cart_product_list')
 
-function agregarCarrito(nombre, precio) {
+function addToCart(name, price) {
 
-    const nuevoItem = document.createElement('li')
-    nuevoItem.classList.add('cart_product_item');
+    const newCartItem = document.createElement('li')
+    newCartItem.classList.add('cart_product_item')
 
-    const nuevoItemName = document.createElement('div')
-    const nuevoItemQuantity = document.createElement('div')
-    const nuevoItemPrice = document.createElement('span')
-    const nuevoItemButton = document.createElement('button')
+    const newCartItemName = document.createElement('div')
+    const newCartItemQuantity = document.createElement('div')
+    const newCartItemPrice = document.createElement('span')
+    const newCartItemButton = document.createElement('button')
 
-    nuevoItemName.classList.add('cart_product_name')
-    nuevoItemQuantity.classList.add('cart_product_quantity')
-    nuevoItemPrice.classList.add('cart_product_price')
-    nuevoItemButton.classList.add('cart_remove_item')
+    newCartItemName.classList.add('cart_product_name')
+    newCartItemQuantity.classList.add('cart_product_quantity')
+    newCartItemPrice.classList.add('cart_product_price')
+    newCartItemButton.classList.add('cart_remove_item')
 
-    nuevoItemName.textContent = nombre
-    nuevoItemQuantity.textContent = 1
-    nuevoItemPrice.textContent = precio
-    // nuevoItemChild3.textContent = 'Quitar'
+    newCartItemName.textContent = name
+    newCartItemQuantity.textContent = 1
+    newCartItemPrice.textContent = price
+    // newCartItemChild3.textContent = 'Quitar'
 
-    nuevoItem.appendChild(nuevoItemName)
-    nuevoItem.appendChild(nuevoItemQuantity)
-    nuevoItem.appendChild(nuevoItemPrice)
-    nuevoItem.appendChild(nuevoItemButton)
+    newCartItem.appendChild(newCartItemName)
+    newCartItem.appendChild(newCartItemQuantity)
+    newCartItem.appendChild(newCartItemPrice)
+    newCartItem.appendChild(newCartItemButton)
 
-    cartContainer.appendChild(nuevoItem)
-
-    // Añadimos al local storage
-    carrito.push({nombre, precio})
-    guardarCarrito(carrito)
+    cartContainer.appendChild(newCartItem)
 
     // Recalculamos el total
-    const total = calcularTotal()
-    totalCarrito.textContent = total.toFixed(2)
+    const total = calculateTotal()
+    cartTotal.textContent = total.toFixed(2)
 }
 
 // Acción para escuchar el botón de "Añadir al carrito"
 
-const botonAgregar = document.querySelectorAll('.card_submit')
+const addButton = document.querySelectorAll('.card_submit')
+const cartItems = document.querySelectorAll('.cart_product_item')
 
-botonAgregar.forEach(boton => {
+addButton.forEach(boton => {
     boton.addEventListener('click', function() {
-        const card = boton.closest('.card');
-        const nombreProducto = card.querySelector('.card_title').textContent;
-        const precioProducto = card.querySelector('.card_price').textContent;
+        const card = boton.closest('.card')
+        const itemName = card.querySelector('.card_title').textContent
+        const itemPrice = card.querySelector('.card_price').textContent
 
-        agregarCarrito(nombreProducto, precioProducto)
-    });
-});
+        const cartItems = document.querySelectorAll('.cart_product_item')
+
+        const existingCartItem = [...cartItems].find(item => {
+            const cartItemName = item.querySelector('.cart_product_name').textContent
+            return (cartItemName == itemName) ? true : false
+        })
+
+        // Si existe el elemento en el carrito añadimos +1 a la cantidad e incrementamos el precio
+        if (existingCartItem) {
+            const itemQuantity = existingCartItem.querySelector('.cart_product_quantity')
+            const currentQuantity = parseInt(itemQuantity.textContent, 10)
+            itemQuantity.textContent = currentQuantity +1
+
+            const itemTotalPrice = existingCartItem.querySelector('.cart_product_price')
+            const currenTotalPrice = parseFloat(itemTotalPrice.textContent.replace(',', ''))
+            const newTotalPrice = (currentQuantity + 1) * itemPrice
+            itemTotalPrice.textContent = newTotalPrice.toFixed(2)
+
+        } else {
+            addToCart(itemName, itemPrice)
+        }
+
+        // Recalculamos el total
+        const total = calculateTotal()
+        cartTotal.textContent = total.toFixed(2)
+    })
+})
 
 // Eliminar del carrito
 
 cartContainer.addEventListener('click', function(event) {
     if (event.target.classList.contains('cart_remove_item')) {
-        const item = event.target.closest('.cart_product_item');
-        const index = Array.from(cartContainer.children).indexOf(item);
+        const item = event.target.closest('.cart_product_item')
+        const index = Array.from(cartContainer.children).indexOf(item)
 
-        item.remove();
+        item.remove()
 
         // Recalculamos el total
-        const total = calcularTotal()
-        totalCarrito.textContent = total.toFixed(2)
+        const total = calculateTotal()
+        cartTotal.textContent = total.toFixed(2)
 
-        // Eliminamos del local storage
-        carrito.splice(index, 1);
-        console.log(carrito)
-        guardarCarrito(carrito); 
     }
-});
+})
 
 // Suma el total de todos los elementos que haya en el carrito.
 
-let totalCarrito = document.getElementById('cart_total')
+let cartTotal = document.getElementById('cart_total')
 
-function calcularTotal(){
-    const precioItemsCarrito = document.querySelectorAll('.cart_product_price')
+function calculateTotal(){
+    const cartItemsPrice = document.querySelectorAll('.cart_product_price')
     let total = 0
-    precioItemsCarrito.forEach(item => {
-        total += parseFloat(item.textContent.replace('$', '').replace(',', ''));
-    });
+    cartItemsPrice.forEach(item => {
+        total += parseFloat(item.textContent.replace(',', ''))
+    })
 
     return total
-};
-
-const total = calcularTotal()
-totalCarrito.textContent = total.toFixed(2)
-
-// Cargamos el carrito cuando se carga la página
-
-function cargarCarrito() {
-    while (cartContainer.firstChild) {
-        cartContainer.removeChild(cartContainer.firstChild);
-    }
-
-    const carritoEnLocalStorage = obtenerCarrito();
-    carrito = [];
-
-    carritoEnLocalStorage.forEach(item => {
-        // Comprobar si el producto ya existe en el carrito
-        const existeEnCarrito = carrito.some(cartItem => cartItem.nombre === item.nombre);
-
-        if (!existeEnCarrito) {
-            agregarCarrito(item.nombre, item.precio);
-        }
-    });
 }
 
-let carritoCargado = false;
-
-document.addEventListener("DOMContentLoaded", function() {
-    if (!carritoCargado) {
-        cargarCarrito();
-        carritoCargado = true;
-    }
-});
+const total = calculateTotal()
+cartTotal.textContent = total.toFixed(2)
